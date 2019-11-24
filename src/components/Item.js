90bb55be24d825abed84aca47e85213a64b85696
Ofoe <ofoefiergbor16@gmail.com>
2019-11-24 11:37:18 +0000

    changed the position of the orange dash

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import shirt from '../images/shirt.jpg'
+
 
 const Item =(props)=>{
     console.log(props)
@@ -12,7 +12,7 @@ const Item =(props)=>{
                     <div className="text text-center"><p>Free Shipping</p></div>
                     <div className="caption caption-custom text-center">
                         <h5>Cat Tee Summer T-shirt</h5>
-                        <div className="dash"></div>
+                        <center><div className="dash"></div></center>
                         <div><p className="text-center"><span>$</span>10.<span>90</span> <br/> <span>or 9 x$1.21</span></p></div>
                         <p><a href="#" className="btn btn-primary btn-custom" role="button">Add to Cart</a></p>  
                     </div>
